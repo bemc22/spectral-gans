a20a3a217959013260e73513f0d27ac77fa2e5ec
Brayan Monroy <brayansneider22@gmail.com>
2021-08-29 20:18:30 -0500

    training loop

--- models/main.py
+++ models/main.py
@@ -5,39 +5,26 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
 from tensorflow.keras.losses import Loss , MSE
 
-class loss1(Loss):
-
-    def call(self, y_true, y_pred):
-
-        loss = tf.math.log(y_pred)
-        return - tf.reduce_mean(loss)
-
-class loss2(Loss):
-
-    def call(self, y_true, y_pred):
-
-        loss = tf.math.log(1 - y_pred)
-        return - tf.reduce_mean(loss)
 
 
-def SpectralGAN(input_shape, features=64, factors=FACTOR):
-
+def make_autoencoder(input_shape, features=64, factors=FACTOR):
     out_dim = input_shape[-1]
     _input = Input(input_shape)
-
     encoder = Encoder(features=features, factors=FACTOR)
     generator = Generator(out_dim, features=features, factors=FACTOR)
-    discriminator = Discriminator(features=features, factors=FACTOR)
 
-    z = encoder(_input)
+    z = encoder(_input)    
     _output = generator(z)
-
-    real = discriminator(_input)
-    fake = discriminator(_output)
-
-
-    model = Model( _input , [_output, real, fake] , name='SpectralGAN' )
+    model = Model( _input , _output , name='generator' )
     return model
+
+def make_discriminator(input_shape, features=64, factors=FACTOR):
+    _input = Input(input_shape)
+    discriminator = Discriminator(features=features, factors=FACTOR)
+    _output = discriminator(_input)
+    model = Model( _input , _output , name='discriminator' )
+    return model
+
 
 
 
