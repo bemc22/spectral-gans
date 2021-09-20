41327c003c1072928802cc58ae1b0d76079f5e58
Brayan Monroy <brayansneider22@gmail.com>
2021-09-20 10:01:03 -0500

    layers upate

--- models/layers.py
+++ models/layers.py
@@ -1,47 +1,59 @@
 import tensorflow as tf
 from tensorflow.keras.layers import *
+from tensorflow.keras.regularizers import L2
 
+FACTOR = [1, 1, 1/2, 1/2, 1/4]
 
-FACTOR = [1 , 1 , 1/2 , 1/2 , 1/4 ]
 
 class convBlock(tf.keras.layers.Layer):
-    def __init__(self, filters=64, batch_norm=True, activation='relu'):
+    def __init__(self, filters=64, normalize=False, activation='relu'):
         super(convBlock, self).__init__()
 
-        conv = Conv2D(filters, 3, activation=None, padding="same")
+        conv = Conv2D(filters, 3,
+                      activation=None,
+                      padding="same",
+                      kernel_regularizer=L2(1e-8))
+
         activation = Activation(activation)
 
         self.layers = [conv]
-
-        if batch_norm:
-            bn = BatchNormalization()
-            self.layers.append(bn)
-        
         self.layers.append(activation)
+
+        if normalize:
+            bn = LayerNormalization()
+            self.layers.append(bn)
+
         
-        
+
     def call(self, inputs):
         x = inputs
         for layer in self.layers:
-            x = layer(x)  
+            x = layer(x)
         return x
+
 
 class Encoder(tf.keras.layers.Layer):
     def __init__(self, features=64, factors=FACTOR):
         super(Encoder, self).__init__()
 
         self.convs = []
-        for factor in factors:
+
+        for factor in factors[:-1]:
             feature = int(factor*features)
             conv = convBlock(feature)
             self.convs.append(conv)
 
+        feature = int(factors[-1]*features)
+        conv = convBlock(feature, activation=None, normalize=False)
+        self.convs.append(conv)
+
     def call(self, inputs):
+
         x = inputs
         for conv in self.convs:
             x = conv(x)
-    
         return x
+
 
 class Generator(tf.keras.layers.Layer):
     def __init__(self, out_dim, features=64, factors=FACTOR):
@@ -53,30 +65,27 @@ class Generator(tf.keras.layers.Layer):
             feature = int(factor*features)
             conv = convBlock(feature)
             self.convs.append(conv)
-        
-        conv = convBlock(out_dim, batch_norm=False)
+
+        conv = convBlock(out_dim, activation='relu', normalize=False)
         self.convs.append(conv)
-    
+
     def call(self, inputs):
+
         x = inputs
         for conv in self.convs:
             x = conv(x)
-    
         return x
+
 
 class Discriminator(tf.keras.layers.Layer):
     def __init__(self, features=64, factors=FACTOR):
         super(Discriminator, self).__init__()
 
         self.encoder = Encoder(features, factors)
-        self.last_conv = convBlock(1, batch_norm=False, activation='sigmoid')
+        self.last_conv = convBlock(1, normalize=False, activation='sigmoid')
 
     def call(self, inputs):
 
         x = self.encoder(inputs)
         x = self.last_conv(x)
-
         return x
-
-
-
