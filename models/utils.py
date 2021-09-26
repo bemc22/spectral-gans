c98cb98c395874c3da8435e773332e7b9d99247a
Brayan Monroy <brayansneider22@gmail.com>
2021-09-26 10:04:15 -0500

    edit

--- models/utils.py
+++ models/utils.py
@@ -5,7 +5,7 @@ def discriminator_loss(real_output, fake_output):
     loss = tf.math.log(real_output) + tf.math.log(1 - fake_output)
     return - tf.math.reduce_mean(loss)
     
-def autoencoder_loss(real_x, estimated_x, real_output, fake_output, tau=0.001):
+def autoencoder_loss(real_x, estimated_x, real_output, fake_output, tau=1e-4):
     autoencoder_loss = mean_squared_error(real_x, estimated_x)
     gan_loss =  tau*discriminator_loss(real_output, fake_output)
     total_loss = autoencoder_loss - gan_loss
